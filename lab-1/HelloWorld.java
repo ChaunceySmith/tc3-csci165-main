59d5199a49d88d3202def089c4f0c12247b03f54
Chauncey Smith <chaunceysmith@myjacs.org>
2020-01-31 02:13:31 -0500

    Submission for Lab

---
+++ lab-1/HelloWorld.java
@@ -0,0 +1,17 @@
+//Chauncey Smith
+//Lab one
+// First intro to java and Helloworld assignment
+
+
+//file name must match the public class identifier
+public class HelloWorld{
+
+    //main method is the starting point of any Java app
+    public static void main(String[] args){
+
+      for (String s : args)
+
+        //built in terminal printing utility
+        System.out.println("String Arguments: " + s);
+    }  //end of main
+} //end of class
---
+++ lab-1/HelloWorld2.java
@@ -0,0 +1,11 @@
+//Chauncey Smith
+
+//File name must math the public class identifier
+public class HelloWorld2{   //must match the file name
+
+    public static void main(String[] args){   //set the class and tell the object type
+
+        //set args as 0 1 so the user can type any name and have the name appear in message
+      System.out.println("Hello " + args[0] + " " + args[1] + ", you should spend more time studying java becasue it took you a long time to code this.");
+    }//end main
+}//end class
