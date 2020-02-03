928d330f612f472b9f41d0151ff1221ba8c3b56e
Chauncey Smith <chaunceysmith@myjacs.org>
2020-02-03 11:14:40 -0500

    Commiting week 2 and lab stufff

--- week-2/lab/group_work/Tickets.java
+++ week-2/lab/group_work/Tickets.java
@@ -1,8 +1,16 @@
+//Chauncey Smith, Garett Davenport
+//Tickets Group assignment
+
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileInputStream;
+
 public class Tickets{
 
     public static void main(String[] args){
 
-        /* 
+        /*
             The following code steps you through the String processing
             and math neccessary to determine if a single ticket number is valid.
             Your job with this code is to:
@@ -25,18 +33,39 @@ public class Tickets{
             Check your work: There are 101 valid ticket numbers among the 1000 provided ticket numbers
                              These people make mistakes!!!
 
-            Submit only Java source code files. Also submit valid_tix.txt   
+            Submit only Java source code files. Also submit valid_tix.txt
 
         */
+        FileInputStream fileIn = null;
+        Scanner fileScan = null;
+        try{
+            fileIn = new FileInputStream("tickets.txt"); //first we use a try except (we do this to open the file)
+            FileWriter fileWriter = new FileWriter("valid_tix.txt");
+            fileScan = new Scanner(fileIn); //next we create a scanner to read through the file, we need this becasue we have to go through the tickets
+            int counter = 0;   //we set the line number to help the scanner go along each line of the file
+            while(fileScan.hasNextLine()){ //now we set a while loop for the file scanner to go through every line
+                String ticket = fileScan.nextLine();
+                String  last = ticket.substring(ticket.length() - 1);  // also a string setting a variable called last, we are removing the last digit of the string(taking the length -1)
+                int     last_digit = Integer.valueOf(last);  //we are now storing the value of the last_digit becasue we need it to compare to the remainder, so we are simply setting a variable
+                String  reduced_ticket = ticket.substring(0, ticket.length() - 1);
+                int     ticket_number = Integer.valueOf(reduced_ticket); //this is our final int of the reduced ticket without the last digit
+                int     remainder = ticket_number % 7; //using modulus division to find the remainder of the ticket number % by 7
+                boolean validity = remainder == last_digit; //this is how we check the validity of the statement, if the remainder == the last digit the ticket is valid
 
-        String  ticket  = "123454";                            
-        String  last = ticket.substring(ticket.length() - 1);                                                   
-        int     last_digit = Integer.valueOf(last);
-        String  reduced_ticket = ticket.substring(0, ticket.length() - 1);
-        int     ticket_number = Integer.valueOf(reduced_ticket);
-        int     remainder = ticket_number % 7; 
-        boolean validity = remainder == last_digit; 
-        String  format = "Original Ticket #: %s\nLast Digit: %d\nReduced Ticket #: %d\nRemainder: %d\nValidity: %b\n";
-        System.out.printf(format, ticket, last_digit, ticket_number, remainder, validity);  
-    }
-}
+                if(validity == true){ //we need an if statement so we can write the ticket number if it is valid
+
+                    fileWriter.write(ticket + '\n'); //writing the ticket to the file
+                    counter++;}
+
+            }//end of while loop
+            System.out.println(counter);  //counting the digits 
+            fileScan.close();  //closing a file
+            fileWriter.close(); //closing the fileWriter
+        }//end of try
+        catch(IOException e){
+            System.out.println("File not Found");
+        }//end of catch for FileInputStream
+      //String  ticket  = "123454";   //telling Java what ticket it is, stating it is a string
+
+    }//end of main
+}//end of class
